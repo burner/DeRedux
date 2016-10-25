d2533db1da589bd36b6db176a81a3f3e37bff8fa
Robert burner Schadek <rburners@gmail.com>
2016-10-25 14:36:01 +0200

    most stuff works

--- source/deredux.d
+++ source/deredux.d
@@ -15,28 +15,34 @@ struct ImmuWrapper(T) {
 	}
 }
 
-struct Foo {
-	int value;
-}
+struct Data(T) {
+	import core.sync.mutex;
+	immutable(T)* data;
+	Mutex* muPtr;
 
-struct FooRedux {
-	Foo fun(const(Foo) foo) {
-		return cast(Foo)foo;
+	this(immutable(T)* data, shared(Mutex*) muPtr) {
+		this.data = data;
+		this.muPtr = cast(Mutex*)muPtr;
 	}
-}
 
-Foo bar(const(Foo) foo) {
-	return cast(Foo)foo;
-}
+	this() @disable;
 
-unittest {
-	import std.stdio;
+	~this() {
+		(*this.muPtr).unlock();
+	}
 
-	auto fooState = State!(Foo)(Foo(1337));
-	fooState.exe(&bar);
+	this(this) @disable {}
+	void opAssign(T)(T other) @disable {}
 
-	FooRedux fr;
-	fooState.exe(&fr.fun);
+	alias get this;
+
+	ref immutable(T) get() {
+		return *this.data;
+	}
+
+	ref immutable(T) opCast(T)() {
+		return *this.data;
+	}
 }
 
 struct State(Type) {
@@ -56,9 +62,56 @@ struct State(Type) {
 	}
 
 	void exe(F,Args...)(F f, Args args) {
+		Mutex mu = cast(Mutex)this.mutex;
+		mu.lock();
+		scope(exit) mu.unlock();
+
 		if(this.state.length + 1 == this.state.capacity()) {
 			this.state.removeFront();
 		}
 		this.state.insertBack(ImmuWrapper!Type(f(this.state.back.immu, args)));
 	}
+
+	Data!(Type) peek() {
+		Mutex mu = cast(Mutex)this.mutex;
+		mu.lock();
+		return Data!(Type)(&this.state.back.immu, &this.mutex);
+	}
+}
+
+struct Foo {
+	int value;
+}
+
+struct FooRedux {
+	Foo fun(const(Foo) foo) {
+		return Foo(foo.value + 2);
+	}
+}
+
+Foo bar(const(Foo) foo) {
+	return Foo(foo.value + 1);
+}
+
+unittest {
+	import std.stdio;
+	import exceptionhandling;
+
+	const begin = 1337;
+
+	auto fooState = State!(Foo)(Foo(begin));
+
+	int cnt = 126;
+	for(int i = 1; i <= cnt; ++i) {
+		fooState.exe(&bar);
+		cast(void)assertEqual(fooState.peek().value, begin + i);
+	}
+
+	cast(void)assertEqual(fooState.peek().value, begin + cnt);
+
+	auto f = fooState.peek();
+	pragma(msg, typeof(f));
+
+	FooRedux fr;
+	fooState.exe(&fr.fun);
 }
